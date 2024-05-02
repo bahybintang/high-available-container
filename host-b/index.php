c944d6fad678be6fec2249b9fcdbf89951a79b1f
Muhammad Bintang Bahy <bahybintang@gmail.com>
2024-05-02 19:50:12 +0700

    Update PHP code to show local IP

--- host-b/index.php
+++ host-b/index.php
@@ -1,5 +1,3 @@
 <?php
-    echo "Requested IP\t:" . $_SERVER['SERVER_ADDR'];
-    echo "\nLocal IP\t:" . exec("ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1");
-?>
-
+echo "Requested IP\t:" . $_SERVER['SERVER_ADDR'];
+echo "\nLocal IP\t:" . exec("ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | head -n 1");
